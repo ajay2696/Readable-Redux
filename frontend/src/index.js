6899f556db6c2d9fc0f3748481f80002796f0f37
Ajay Nara <33077370+ajay2696@users.noreply.github.com>
2018-04-12 01:06:58 -0400

    Fix: Removed Unwanted Code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,23 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import Redux,{createStore,applyMiddleware,compose} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import AppReducer from './reducers/AppReducer';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import {logger} from 'redux-logger';
 import thunk from 'redux-thunk';
-/*
-const logger=store=>next=>action=>{
-    console.group(action.type);
-    console.info('dispatching',action);
-    let result=next(action);
-    console.log('next state',store.getState());
-    console.groupEnd(action.type);
-    return result;
-}
-*/
+
 const composeEnhacers=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose;
 
 const store = createStore(
